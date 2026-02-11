4fe1d4b865e51242b394c002ae9228274fe1e4b7
AJ-AYUSHMAN <sonurajsheohar@gmail.com>
2026-02-11 22:32:47 +0530

    updated

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
-import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import ListScreen from './Screens/Listscreen';
+import ListScreen from "./Screens/ListScreen";
 import DetailScreen from "./Screens/DetailScreen";
+import FormScreen from "./Screens/FormScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Products" component={ListScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
+
+        <Stack.Screen
+          name="List"
+          component={ListScreen}
+        />
+
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+        />
+
+        <Stack.Screen
+          name="Form"
+          component={FormScreen}
+        />
+
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
